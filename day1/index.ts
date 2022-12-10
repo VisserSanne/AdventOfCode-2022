1bae6e5d32351b4b734c2109c2ce5f465fc72445
VisserSanne <sannesvisser@gmail.com>
2022-12-10 19:07:41 +0100

    Complete day 1

--- day1/index.ts
+++ day1/index.ts
@@ -1,1 +1,42 @@
-console.log('test');
+import * as fs from 'fs';
+const input = fs.readFileSync('./day1/input.txt','utf8').split('\n\n');
+
+let elfWithMostCal = 0;
+let calsOfElf = 0;
+
+function partOne() {
+    input.map((elfCalories, id) => {
+        let calories = 0;
+        elfCalories.split('\n').map(value => calories += parseInt(value));
+        
+        if (calories > calsOfElf) {
+            calsOfElf = calories;
+            elfWithMostCal = id;
+        }
+    })
+}
+
+
+let topThree = [0,0,0];
+let totalTopThree = 0;
+
+function partTwo() {
+    input.map((elfCalories, id) => {
+        let calories = 0;
+        elfCalories.split('\n').map(value => calories += parseInt(value));
+        
+        if (calories > topThree[0]) {
+            topThree[2] = topThree[1];
+            topThree[1] = topThree[0];
+            topThree[0] = calories;
+        } else if (calories > topThree[1]) {
+            topThree[2] = topThree[1];
+            topThree[1] = calories;
+        } else if (calories > topThree[2]) {
+            topThree[2] = calories;
+        }
+    })
+    topThree.map(calories => totalTopThree += calories);
+}
+
+partTwo();
